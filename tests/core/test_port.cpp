d9822f0134a365c799329f16248d2fe46d5af7ae
World Builder <93953323+redvampir@users.noreply.github.com>
2025-11-14 20:25:42 +0300

    Fix display name test guard

--- tests/core/test_port.cpp
+++ tests/core/test_port.cpp
@@ -161,15 +161,11 @@ TEST_CASE("Port: Empty name", "[port][edge]") {
     REQUIRE(port.get_name().empty());
 }
 
-#if 0  // Display name feature not yet implemented
+#if defined(VISPROG_ENABLE_PORT_DISPLAY_NAME_TEST)
 TEST_CASE("Port: Display name", "[port]") {
     Port port(PortId{1}, PortDirection::Input, DataType::Int32, "value");
 
     REQUIRE(port.get_name() == "value");
 }
-#endif
-
-
-    REQUIRE(port.get_name() == "value");
-}
 #endif  // VISPROG_ENABLE_PORT_DISPLAY_NAME_TEST
+
